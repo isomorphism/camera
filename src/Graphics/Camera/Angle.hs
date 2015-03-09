514044fe8f1ac80da2f63f6fcf1ff60316fd155c
C. A. McCann <cam@uptoisomorphism.net>
2015-03-08 23:43:09 -0400

    add some haddocks

--- src/Graphics/Camera/Angle.hs
+++ src/Graphics/Camera/Angle.hs
@@ -5,26 +5,31 @@ module Graphics.Camera.Angle where
 import Foreign.Storable
 
 import Control.Lens
-import Numeric.Lens
 
-import Linear
 
-newtype Angle a = 
-    Angle a
+
+-- | Wrapper type representing an angle.
+newtype Angle a = Angle a
   deriving (Eq, Ord, Read, Show, Storable, Num, Fractional)
 
-
+-- | Construct an angle using an angle unit isomorphism, e.g.:
+--
+--   @'angleFrom' 'degrees' 60@ ≡ @'angleFrom' 'turns' (1/6)@ ≡ @'angleFrom' 'radians' ('pi'/3)@
 angleFrom :: AnIso' (Angle a) a -> a -> Angle a
 angleFrom l = view . from $ l
 
+-- | Converts between abstract 'Angle's and "turns", or fractions of a complete circle.
 turns :: Iso' (Angle a) a
 turns = iso (\(Angle a) -> a) Angle
 
+-- | Converts between abstract 'Angle's and radians.
 radians :: (Floating a) => Iso' (Angle a) a
 radians = turns . iso (* τ) (/ τ)
 
+-- | Converts between abstract 'Angle's and degrees. 
 degrees :: (Fractional a) => Iso' (Angle a) a
 degrees = turns . iso (* 360) (/ 360)
 
+-- | The circle constant, relating turns to radians.
 τ :: (Floating a) => a
 τ = 2 * pi
